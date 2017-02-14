3cadcbe9eff92d07435501f9e993ecc39268efa5
unknown <zhe.xiao@qunarservers.com>
2017-02-14 18:40:01 +0800

    c

--- testExe/textExecutive.py
+++ testExe/textExecutive.py
@@ -3,11 +3,13 @@
 import test
 import unittest
 from baseCase.baseSuite import baseLoder
+from baseCase.baseRun import baseRun
 
 
 def test_executive():
     test_case = baseLoder.loadTestsFromModule(test)
-    unittest.TextTestRunner( verbosity=2).run(test_case)
+    # unittest.TextTestRunner( verbosity=2).run(test_case)
+    baseRun.run(test_case)
 
 if __name__ == "__main__":
     test_executive()

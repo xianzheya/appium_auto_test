10d2d6bfc78aea209a50fe7decf04fbf3b3d0fa3
zhe <836333664@example.com>
2017-02-15 18:15:20 +0800

    node.js appium

--- test/test_1.py
+++ test/test_1.py
@@ -1,9 +1,9 @@
 # -*- coding:utf-8 -*-
 
-# from test.baseTest import BaseTest
 from baseCase.case import BaseTest
-
+from server.log import LoggerBase
 
 class test(BaseTest):
     def  test1(self):
         self.assertEquals(1, 1)
+        LoggerBase().info("xz")
--- testExe/textExecutive.py
+++ testExe/textExecutive.py
@@ -3,13 +3,11 @@
 import test
 import unittest
 from baseCase.baseSuite import baseLoder
-from baseCase.baseRun import BaseRun
 
 
 def test_executive():
     test_case = baseLoder.loadTestsFromModule(test)
-    # unittest.TextTestRunner( verbosity=2).run(test_case)
-    BaseRun("txt.txt").run(test_case)
+    unittest.TextTestRunner(verbosity=2).run(test_case)
 
 if __name__ == "__main__":
     test_executive()

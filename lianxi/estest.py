754fd924a77abc8a9e4687b7fc6e6f50b73f7a6f
xiaoyaojushi <836333664@qq.com>
2018-10-25 16:03:50 +0800

    Update estest.py

--- lianxi/estest.py
+++ lianxi/estest.py
@@ -4,6 +4,6 @@ from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 from pprint import pprint
 
-es = Elasticsearch(hosts='l-activity.wap.beta.cn0.qunar.com:9200')
+es = Elasticsearch(hosts='')
 res = es.get(index="vacation", doc_type="vacationDetail", id="_search")
 pprint(res)
